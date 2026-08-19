05db870d17964616f22987a239f9f5e223aa5848
phananh-123 <phana0202@gmail.com>
2026-08-20 01:07:33 +0700

    push

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -60,7 +60,7 @@ function Profile() {
             Figma
           </a>
 
-          <a href="https://www.figma.com/design/30pKkVf4buzh3VOmgiMQsE/Untitled?node-id=0-1&t=Omhpo6eZ8bwDz08R-1" target="_blank">
+          <a href="https://anhp-b23dcat018-7917810.postman.co/workspace/B23DCAT018---Phan-Anh's-Workspa~5e70cd83-27b7-4c28-8f10-6f3bb24e45be/collection/57565573-eb6507f7-0ae5-45fd-b1a6-21e04c5c03cd?action=share&source=copy-link&creator=57565573" target="_blank">
             Postman API
           </a>
 
